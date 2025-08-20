47af7dd9d448b95072a078da62e4324f672dc56d
summit1123 <ruggy245@naver.com>
2025-08-21 00:20:57 +0900

    refactor: 디자인 수정

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -25,48 +25,25 @@ export default function StartPage() {
         </div>
       )}
 
-      {/* 오른쪽 하단 종이비행기 */}
-      <div className="absolute bottom-16 right-16 animate-pulse">
-        <svg
-          className="w-24 h-24 text-[#6ba3ff] transform hover:scale-110 transition-transform duration-300"
-          viewBox="0 0 64 64" fill="none" stroke="currentColor"
-          strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
-        >
-          <path d="M60 6 26 38l6 20 8-14 20-38Z" />
-          <path d="M60 6 4 26l20 6" />
-        </svg>
-      </div>
-
-      {/* 하단 웨이브 - 왼쪽에서 오른쪽으로 쭉 뻗은 선 (197번 이미지 스타일) */}
-      <svg
-        className="absolute bottom-0 left-0 w-full h-32 text-[#6ba3ff]"
-        viewBox="0 0 1200 120" 
-        preserveAspectRatio="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0,80 C200,40 400,100 600,60 C800,20 1000,80 1200,40"
-          stroke="currentColor" strokeWidth="2" fill="none" opacity="0.6"
+      {/* 하단 웨이브 라인과 종이비행기 */}
+      <div className="absolute bottom-0 left-0 w-full">
+        <img 
+          src={require('../assets/images/wave-airplane.png')}
+          alt="Wave and Airplane"
+          className="w-full h-auto"
         />
-      </svg>
+      </div>
       
       {/* 중앙 콘텐츠 */}
       <div className="relative z-10 flex flex-col items-center text-center px-6">
         {/* MEARI 로고 아이콘 */}
         <div className="mb-6">
-          <svg
-            className="w-20 h-20 text-white"
-            viewBox="0 0 100 100"
-            fill="currentColor"
-          >
-            <path d="M50 20 L30 40 L40 50 L50 45 L60 50 L70 40 Z" />
-            <path d="M25 35 Q15 25 25 15 Q35 25 25 35" />
-            <path d="M75 35 Q85 25 75 15 Q65 25 75 35" />
-          </svg>
+          <img 
+            src={require('../assets/images/meari-logo.png')}
+            alt="MEARI Logo"
+            className="w-20 h-auto"
+          />
         </div>
-        
-        {/* MEARI 텍스트 */}
-        <h1 className="text-4xl font-bold text-white mb-8 tracking-wider">MEARI</h1>
 
         {/* 서브 카피 */}
         <div className="text-center mb-12 max-w-md">
